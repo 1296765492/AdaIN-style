82a79b47bc89ab3126c3cf9fe77684edaa966953
1296765492 <1296765492@qq.com>
2021-05-26 13:03:08 +0000

    wikiart style images added

---
+++ kaggle.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#kaggle competitions download -c painter-by-numbers -f train_1.zip
+#kaggle competitions download -c painter-by-numbers -f train_2.zip
+#kaggle competitions download -c painter-by-numbers -f train_3.zip
+#kaggle competitions download -c painter-by-numbers -f train_4.zip
+#kaggle competitions download -c painter-by-numbers -f train_5.zip
+#kaggle competitions download -c painter-by-numbers -f train_6.zip
+#kaggle competitions download -c painter-by-numbers -f train_7.zip
+#kaggle competitions download -c painter-by-numbers -f train_8.zip
+#kaggle competitions download -c painter-by-numbers -f train_9.zip
+
+#unzip -q train_1.zip
+#unzip -q train_2.zip
+#unzip -q train_3.zip
+#unzip -q train_4.zip
+#unzip -q train_5.zip
+#unzip -q train_6.zip
+#unzip -q train_7.zip
+#unzip -q train_8.zip
+#unzip -q train_9.zip
+
+#mv train_1/*.* wikiart
+mv train_2/*.* wikiart
+mv train_3/*.* wikiart
+mv train_4/*.* wikiart
+mv train_5/*.* wikiart
+mv train_6/*.* wikiart
+mv train_7/*.* wikiart
+mv train_8/*.* wikiart
+mv train_9/*.* wikiart
