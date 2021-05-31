a68cd39be1d86f8e91a040771c0ed2cfc703c7ba
1296765492 <chengqianjin98@gmail.com>
2021-05-31 11:36:21 +0800

    print feature maps' content

--- lib/AdaptiveInstanceNormalization.lua
+++ lib/AdaptiveInstanceNormalization.lua
@@ -48,27 +48,20 @@ function AdaptiveInstanceNormalization:updateOutput(input) --{content, style}
     
     -- helper
     print(N, self.nOutput, Hc, Wc, Hs, Ws)
-    -- 4 dimensions only
-    for k1, v1 in pairs(content) do
-      print(k1)
-      for k2, v2 in pairs(content[k1]) do
-        print(k2)
-        for k3, v3 in pairs(content[k1][k2]) do
-          print(k3)
-          for k4, v4 in pairs(content[k1][k2][k3]) do
-            print(k4, v4)
+    for n = 1, N do 
+      for c = 1, self.nOutput do
+        for h = 1, Hc do
+          for w = 1, Wc do
+            print(content[n][c][h][w])
           end
         end
       end
     end
-    for k1, v1 in pairs(style) do
-      print(k1)
-      for k2, v2 in pairs(style[k1]) do
-        print(k2)
-        for k3, v3 in pairs(style[k1][k2]) do
-          print(k3)
-          for k4, v4 in pairs(style[k1][k2][k3]) do
-            print(k4, v4)
+    for n = 1, N do 
+      for c = 1, self.nOutput do
+        for h = 1, Hs do
+          for w = 1, Ws do
+            print(style[n][c][h][w])
           end
         end
       end
